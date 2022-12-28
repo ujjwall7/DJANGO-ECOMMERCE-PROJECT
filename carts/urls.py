d03d454ccdc8990573e5eee2044dd89eeae26f2d
ujjwal <ujjwal@gftpl.in>
2022-12-28 11:51:52 +0530

    cart functionality

--- carts/urls.py
+++ carts/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.cart,name="cart"),
     path('add/cart/<int:product_id>/', views.add_cart,name="add_cart"),
-    path('remove/cart/<int:product_id>/', views.remove_cart,name="remove_cart"),
-    path('delte/cart/<int:product_id>/', views.delete_cart_item,name="delete_cart"),
+    path('remove/cart/<int:product_id>/<int:cart_item_id>', views.remove_cart,name="remove_cart"),
+    path('delte/cart/<int:product_id>/<int:cart_item_id>', views.delete_cart_item,name="delete_cart"),
   
 ]
 
