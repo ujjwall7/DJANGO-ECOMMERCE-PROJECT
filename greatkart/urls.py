66a88a51527214cdd78fde9cee38080c5989378f
ujjwall7 <sharmaujjwal0921@gmail.com>
2023-11-06 22:15:20 +0530

    add static settings

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -3,11 +3,21 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
 
