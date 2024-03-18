92d893c9711bbc2890ef0a82374733739b043e37
sushant <sushant26070@gmail.com>
2024-03-18 08:33:15 +0530

    Raw code by sushant

--- carts/urls.py
+++ carts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('add/cart/<int:product_id>/', views.add_cart,name="add_cart"),
     path('remove/cart/<int:product_id>/<int:cart_item_id>', views.remove_cart,name="remove_cart"),
     path('delte/cart/<int:product_id>/<int:cart_item_id>', views.delete_cart_item,name="delete_cart"),
+
+    path('checkout/',views.checkout,name='checkout'),
   
 ]
 
